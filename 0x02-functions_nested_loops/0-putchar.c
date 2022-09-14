d7c6583989d01f5f1386919d2df8091ebdc64f32
BamzGod <bamgbade2017@gmail.com>
2022-09-14 08:17:16 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putcahr('\n');
+
+	return(0);
+
+}
+
