d6bb653ace5db4d8b133024b50a7de39fb9efe74
BamzGod <bamgbade2017@gmail.com>
2022-09-19 21:34:18 -0700

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,17 @@
 
 void rev_string(char *s)
 {
+
 int l,
 i;
 char
 ch;
 
 /*find string length without null char*/
-for (l = 0; s[l] != '\0'; ++l);
+for (l = 0; s[l] != '\0'; ++l)
+	;
 /*swap the string by looping to half the string*/
-for (i = 0; i < 1 / 2; ++i)
+for (i = 0; i < l / 2; ++i)
 {
 ch =
 s[i];
@@ -24,4 +26,5 @@ s[i] = s[l - 1 - i];
 /*-1 because the array starts from 0*/
 s[l - 1 - i] = ch;
 }
+
 }
